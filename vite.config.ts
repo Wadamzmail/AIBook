1e66ef3a2c6d4644c5db714a82c21eb63d06bece
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-08-09 13:57:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/AIBook/', // مهم جداً لصفحة GitHub Pages
+        base: './', // مهم جداً لصفحة GitHub Pages
         define: {
             'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
             'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
