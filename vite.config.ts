fea33c7d08f55569153be4e917842e4aa2865924
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-08-09 14:22:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: 'AIBook/', // مهم جداً لصفحة GitHub Pages
+        base: '/AIBook/',
         define: {
             'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
             'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -15,6 +15,6 @@ export default defineConfig(({ mode }) => {
                 '@': path.resolve(__dirname, '.'),
             }
         },
-        plugins: [react()] // عشان يشغل React
+        plugins: [react()]
     };
 });
